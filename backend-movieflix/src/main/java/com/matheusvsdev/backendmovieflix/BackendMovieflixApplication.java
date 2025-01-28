d230d16193e187f4fc13703dc9076b73af01e15c
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-01-28 13:41:41 -0300

    first commit backend

---
+++ backend-movieflix/src/main/java/com/matheusvsdev/backendmovieflix/BackendMovieflixApplication.java
@@ -0,0 +1,13 @@
+package com.matheusvsdev.backendmovieflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMovieflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMovieflixApplication.class, args);
+    }
+
+}
---
+++ backend-movieflix/src/test/java/com/matheusvsdev/backendmovieflix/BackendMovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusvsdev.backendmovieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMovieflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
