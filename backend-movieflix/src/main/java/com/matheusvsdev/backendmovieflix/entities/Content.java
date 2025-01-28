3b2ba8928b0d0b8ec94a522497376e7a9e55e232
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-01-28 13:50:50 -0300

    Starting by creating abstract class Content

---
+++ backend-movieflix/src/main/java/com/matheusvsdev/backendmovieflix/entities/Content.java
@@ -0,0 +1,40 @@
+package com.matheusvsdev.backendmovieflix.entities;
+
+public abstract class Content {
+    private String imgUrl;
+    private String title;
+    private String description;
+
+    public Content() {
+    }
+
+    public Content(String imgUrl, String title, String description) {
+        this.imgUrl = imgUrl;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
